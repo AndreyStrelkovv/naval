df55f547adc64fe826b8f5ce1ded2cac0f31199b
Andrey Strelkov <andrey.strelkov@so.energy>
2023-04-11 16:51:51 +0100

    some fixes

--- apps/fe-naval/src/plugins/vuetify.ts
+++ apps/fe-naval/src/plugins/vuetify.ts
@@ -1,9 +1,11 @@
 import "@mdi/font/css/materialdesignicons.css"
 import "vuetify/styles"
 import { createVuetify } from "vuetify"
+// import * as allThemes from "@/theme/themes"
 
 export default createVuetify({
   theme: {
     defaultTheme: "novaLightTheme",
+    // ...allThemes,
   },
 })
